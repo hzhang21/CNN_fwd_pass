1880d9be0ebe88bcbb137bd15bc9e1100a027110
samhung19 <sam.hung197@gmail.com>
2018-11-14 13:38:06 -0600

    add boundary checking

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -36,18 +36,19 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     // An example use of these macros:
     // float a = y4d(0,0,0,0)
     // y4d(0,0,0,0) = a
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
-    for (c = 0; c < C; c++) { //input channels
-        for (p = 0; p < K; p++) { //p and q go over KxK filter
-            for (q = 0; q < K; q++) {
-                dot += (x4d(b,c,h+p,w+q) * k4d(m,c,p,q));
+    if (b < B && m < M && h < H_out && w < W_out) {
+    #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+    #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+    #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+        for (c = 0; c < C; c++) { //input channels
+            for (p = 0; p < K; p++) { //p and q go over KxK filter
+                for (q = 0; q < K; q++) {
+                    dot += (x4d(b,c,h+p,w+q) * k4d(m,c,p,q));
+                }
             }
         }
-    }
     y4d(b,m,h,w) = dot;
-
+    }
 
 #undef y4d
 #undef x4d
